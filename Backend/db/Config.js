bc13544105aa6cb306e8db72e750adba57ea6d1b
avfitnes <ashokvarma9636@gmail.com>
2024-11-27 16:42:13 +0530

    Mongo db

--- Backend/db/Config.js
+++ Backend/db/Config.js
@@ -1,2 +1,14 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/Admin-Panel");
+require("dotenv").config();
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB connection successfully");
+  })
+  .catch((e) => {
+    console.error("Error connecting to MongoDB:", e.message);
+  });
